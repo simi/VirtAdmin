89dd55202528d950b729129d2eed283eb9be8e2b
Jaromír Červenka <jaromir@virt.io>
2015-06-13 14:09:05 +0200

    Gem updates and SimpleCov

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
   gem 'spring'
   gem 'faker'
+  gem 'simplecov'
 end
 
 group :test do
--- test/support/sessions_helper.rb
+++ test/support/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   def login_user(user)
     visit login_path
-    fill_in I18n.t('attributes.email'), with: @user.email
+    fill_in I18n.t('attributes.email'), with: user.email
     fill_in I18n.t('attributes.password'), with: 'password'
     click_button I18n.t 'sessions.buttons.log_in'
   end
