476a2039532e096bc19e201898c1f35cafa96241
Jaromír Červenka <jaromir@virt.io>
2015-06-14 17:19:49 +0200

    Better flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,37 +3,18 @@ module ApplicationHelper
     link_to t('sessions.buttons.language'), change_language_path(I18n.locale != :en ? 'en' : 'cs'), class: 'pull-right'
   end
 
-  def flash_alert_class(key)
-    alert_class = case key.to_sym
-                  when :notice
-                    'success'
-                  when :alert
-                    'danger'
-                  when :error
-                    'danger'
-                  when :warning
-                    'warning'
-                  else
-                    'primary'
-                  end
-
-    "alert-#{alert_class}"
-  end
-
-  def flash_icon_class(key)
-    icon_class = case key.to_sym
-                 when :notice
-                   'check'
-                 when :warning
-                   'warning'
-                 when :error
-                   'remove'
-                 when :alert
-                   'remove'
-                 else
-                   'info'
-                 end
-
-    "fa-#{icon_class}"
+  def flash_display_helper(key)
+    case key.to_sym
+    when :notice
+      { alert_class: 'success', icon_class: 'check' }
+    when :warning
+      { alert_class: 'warning', icon_class: 'warning' }
+    when :error
+      { alert_class: 'danger', icon_class: 'remove' }
+    when :alert
+      { alert_class: 'danger', icon_class: 'remove' }
+    else
+      { alert_class: 'primary', incon_class: 'info' }
+    end
   end
 end
